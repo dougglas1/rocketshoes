1843c0e138c5af05f98df51101bd065b15fcc391
dougglas1 <dougglas1994@hotmail.com>
2020-04-06 20:33:23 -0300

    Design

---
+++ src/pages/Home/index.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img 
+          src='https://static.netshoes.com.br/produtos/tenis-asics-gel-equation-9-masculino/90/D18-0650-090/D18-0650-090_detalhe1.jpg?ims=280x280'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img 
+          src='https://static.netshoes.com.br/produtos/tenis-asics-gel-equation-9-masculino/90/D18-0650-090/D18-0650-090_detalhe1.jpg?ims=280x280'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img 
+          src='https://static.netshoes.com.br/produtos/tenis-asics-gel-equation-9-masculino/90/D18-0650-090/D18-0650-090_detalhe1.jpg?ims=280x280'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img 
+          src='https://static.netshoes.com.br/produtos/tenis-asics-gel-equation-9-masculino/90/D18-0650-090/D18-0650-090_detalhe1.jpg?ims=280x280'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img 
+          src='https://static.netshoes.com.br/produtos/tenis-asics-gel-equation-9-masculino/90/D18-0650-090/D18-0650-090_detalhe1.jpg?ims=280x280'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img 
+          src='https://static.netshoes.com.br/produtos/tenis-asics-gel-equation-9-masculino/90/D18-0650-090/D18-0650-090_detalhe1.jpg?ims=280x280'
+          alt='Tênis'
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color='#FFF' /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
